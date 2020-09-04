ddaa626340209244542308d5cbe2c16bcfdf46bc
damion <daander@clemson.edu>
2020-09-04 10:56:47 -0400

    first push

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/*
+    Project Description:
+    - You are incharge of writing a very fast application that will recommend
+         a wine to a customer. You will need to open the csv file attached and parse
+         through it. Ask the user for basic information (name, age, wine preference (burgandy / white / etc)).
+         Once you have the customer information, create an array of the top 10 wines in their preference, 
+         then randomly give them one of the wines with all of the associated information about the wine. 
+         You can find the link to the dataset here: https://www.kaggle.com/zhenyulin/whitewinepricerating 
+
+    - If you want to take this to the next level, rewrite this application in python, then use a trained
+        supervised or unsupervised machine learning model using tensorFlow. If you get it working, email 
+        Damion (daander@clemson.edu)
+
+    - Requirements:
+        - Must be written in C. 
+        - Must use github to store your code and give damion permission to view it
+            (my github is danderson2794)
+        - You have programming freedom to do what you want; however, you should implement the
+            the topics we discussed. Here's a list to as a reminder: 
+                - basic c functions:
+                   printf/fprintf
+                    scanf/fscanf
+                    fileI/O
+                    pointers, double pointers, function pointers. 
+                    allocating memory 1D/2D malloc and calloc
+
+*/
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
